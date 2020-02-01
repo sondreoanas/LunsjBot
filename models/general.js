63d2ec0af3d1df621263cf36732378f8eb72a8a0
sondreoanas <sondreoanas@gmail.com>
2020-02-01 19:24:56 +0100

    init

--- models/general.js
+++ models/general.js
@@ -22,5 +22,21 @@ function get_message(message) {
 	];
 }
 
+function get_welcome_message() {
+	return [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Hi @Sondre! :wave: \n\n" + 
+					"This is *Lunsj* :flag-no: (Lunch) bot\n" + 
+					" It's an app that asks all your coworkers when they are ready for lunch and keeps track of the time\n\n" + 
+					"Ask all your coworkers when they are ready for lunch by typing \n" + 
+					"*/lunsj ask all* or */lunsj ask @username*"
+			}
+		}
+	]
+}
+
 module.exports.get_error_message = get_error_message;
 module.exports.get_message = get_message;
