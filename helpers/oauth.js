ff19f82a307e2104ce22201c46d1d2910c1d952f
sondreoanas <sondreoanas@gmail.com>
2020-02-21 22:00:43 +0100

    Implemented OAuth

--- helpers/oauth.js
+++ helpers/oauth.js
@@ -1,10 +1,36 @@
+require('dotenv').config();
+var admin = require("firebase-admin");
+
+var serviceAccount = require('../lunsjbotdb-firebase-adminsdk-pjlus-faa936b432.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://lunsjbotdb.firebaseio.com"
+});
+
+let db = admin.firestore();
+
 const oauthSuccess = ({ res, oAuthResult }) => {
-    // save oAuthResult in MongoDB or somewhere else permanent since it has all the tokens
-      res.redirect('<successfully_installed_thankyou_page_url>');
+    // save oAuthResult in to db
+    const teamId = oAuthResult.team_id;
+    let docRef = db.collection('OAuth').doc(teamId);
+
+    let dataSet = {
+        'userToken': oAuthResult.access_token,
+        'botToken': oAuthResult.bot.bot_access_token,
+        'botId': process.env.SLACK_APP_ID,
+        'botUserId': oAuthResult.bot.bot_user_id
+    };
+    let dbRes = docRef.set(dataSet);
+    
+    // redirect to successfully installed thankyou page url
+    res.redirect('http://www.example.com');
 };
 
 const oauthError = (error) => {
     // do something about that error to let the user know
+    console.log('oauthError');
+    console.log(error);
 };
 
 const oauthStateCheck = (oAuthState) => {
@@ -12,10 +38,19 @@ const oauthStateCheck = (oAuthState) => {
     return true;
 };
 
-const authorizeFn = ({ teamId, enterpriseId, userId, conversationId }) => {
-    // go to your MongoDB or wherever you've stored the tokens and get the values based on teamId and/or userId
-    return '<your_tokens>';
-};
+const  authorizeFn = (async ({ teamId, enterpriseId, userId, conversationId }) => {
+    let token = {};
+    // check db for token
+    await db.collection('OAuth').get()
+        .then((snapshot) => {
+            snapshot.forEach((doc) => {
+                if (doc.id == teamId) {
+                    token = doc.data();
+                }
+            });
+        });
+    return token;
+});
 
 module.exports.oauthSuccess = oauthSuccess;
 module.exports.oauthError = oauthError;
